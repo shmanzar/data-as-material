99c457d47b1b2c29994cae5f1b7e57a64f89efb1
shmanzar <email@smanzar.com>
2021-02-28 15:56:28 -0500

    t04v01 mouseover added

--- remix/theme-04-colour/hex/app.js
+++ remix/theme-04-colour/hex/app.js
@@ -1,10 +1,5 @@
-// const span_array = document.querySelectorAll('span')
-//     .map(sel => {
-//         
-//         let colour = this.innerText;
-//         this.style.backgroundColor = colour;
-//     })
-    
+
+
 function changeColour(tagName) {
     document.querySelectorAll(tagName).forEach(el => {
         let colour = el.textContent;
@@ -13,8 +8,14 @@ function changeColour(tagName) {
         el.style.fontSize="15px";
         el.style.opacity = 0.95;
         el.style.fontFamily ="Helvetica";
-// document.querySelectorAll('span')[1].style.color="#"+document.querySelectorAll('span')[99].innerText
+        el.onmouseover = function () {
+        this.style.color = "whitesmoke";
+        }
+         el.onmouseout = function () {
+        this.style.color = "transparent";
+        }
     });
 }
 
 changeColour('span')
+
