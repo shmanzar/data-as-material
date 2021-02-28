b099663357b4a693d160be0a5ac25e691bfdc651
shmanzar <email@smanzar.com>
2021-02-27 23:51:42 -0500

    t01v01 fixed alignment

--- remix/theme-01-ml/gpt2/app.js
+++ remix/theme-01-ml/gpt2/app.js
@@ -4,8 +4,8 @@ text.onmouseover = function () {
   indicator.classList.add("indvisible")
   text.innerText =
     `is even more fun than going to San Sebastian, Irún, Hendaye, Biarritz, Bayonne
-or being sick to my stomach on the 
-Travesera de Gracia in Barcelona
+or being sick to my stomach on 
+the Travesera de Gracia in Barcelona
 partly because in your orange shirt you look like a better happier St. Sebastian
 partly because of my love for you, partly because of your love for yoghurt
 partly because of the fluorescent orange tulips around the birches
@@ -34,17 +34,20 @@ and your smile would come back to me again`;}
 text.onmouseout = function () {
 indicator.classList.remove("indvisible")
   text.innerText =
-    `is even more fun than going to San Sebastian, Irún, Hendaye, Biarritz, Bayonne or being sick to my stomach on the 
-    Travesera de Gracia in Barcelona partly because in your orange shirt you look like a better happier St. Sebastian 
-    partly because of my love for you, partly because of your love for yoghurt 
-    partly because of the fluorescent orange tulips around the birches 
-    partly because of the secrecy our smiles take on before people and statuary it is hard to believe when I’m with you 
-    that there can be anything as still as solemn as unpleasantly definitive as statuary when right in front of it
- in the warm New York 4 o’clock light we are drifting back and forth
+    `is even more fun than going to San Sebastian, Irún, Hendaye, Biarritz, Bayonne
+or being sick to my stomach on 
+the Travesera de Gracia in Barcelona
+partly because in your orange shirt you look like a better happier St. Sebastian
+partly because of my love for you, partly because of your love for yoghurt
+partly because of the fluorescent orange tulips around the birches
+partly because of the secrecy our smiles take on before people and statuary
+it is hard to believe when I’m with you that there can be anything as still
+as solemn as unpleasantly definitive as statuary when right in front of it
+in the warm New York 4 o’clock light we are drifting back and forth
 between each other like a tree breathing through its spectacles
 
 and the portrait show seems to have no faces in it at all, just paint
-you suddenly wonder why in the world anyone ever did them 
+you suddenly wonder why in the world anyone ever did them this way
                               
           I look at you and I would rather look at you than all the portraits in the world
 except possibly for the Polish Rider occasionally and anyway it’s in the Frick
