2ae0506431d605c7927b0e0af5af0334b483ae46
shmanzar <email@smanzar.com>
2021-03-08 01:16:11 -0500

    9/15 done

--- remix/theme-05-vecs/days-past/app.js
+++ remix/theme-05-vecs/days-past/app.js
@@ -13,7 +13,7 @@ between each other like a tree breathing through its spectacles
 
 and the portrait show seems to have no faces in it at all, just paint
 you suddenly wonder why in the world anyone ever did them
-                                                                                                              I look
+                             I look
 at you and I would rather look at you than all the portraits in the world
 except possibly for the Polish Rider occasionally and anyway it’s in the Frick
 which thank heavens you haven’t gone to yet so we can go together for the first time
@@ -27,72 +27,14 @@ as the horse
                                it seems they were all cheated of some marvelous experience
 which is not going to go wasted on me which is why I’m telling you about it
 `;
+let doc = nlp(txt);
+doc.verbs().toPastTense();
+console.log(doc.text());
 
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight).parent("#cdiv");
-  noStroke();
-  textSize(22);
-  // textAlign(CENTER, CENTER);
-  loadFont("./Inter-Regular.ttf", nextWord);
-}
+document.querySelector(".btn-tense").addEventListener("click", () => {
+  document.querySelector(".text").innerHTML = doc.text();
+});
 
-// replace one random word in the text
-function nextWord() {
-  // let words = RiTa.tokenize(txt); // split into words
-  let doc = nlp(txt);
-  doc.verbs().toPastTense();
-  console.log(doc.text());
-
-  // loop from a random spot
-  let r = floor(random(0, words.length));
-  for (let i = r; i < words.length + r; i++) {
-    let idx = i % words.length;
-    let word = words[idx].toLowerCase();
-    if (word.length < 3) continue; // len >= 3
-
-    // find related words
-    let pos = RiTa.tagger.allTags(word)[0];
-    let rhymes = RiTa.rhymes(word, { pos });
-    let sounds = RiTa.soundsLike(word, { pos });
-    let spells = RiTa.spellsLike(word, { pos });
-    let similars = [...rhymes, ...sounds, ...spells];
-
-    // only words with 2 or more similars
-    if (similars.length < 2) {
-      console.log("No substitute for " + word);
-      continue;
-    }
-
-    // pick a random similar
-    let next = RiTa.random(similars);
-
-    if (next.includes(word) || word.includes(next)) {
-      continue; // skip substrings
-    }
-    if (/[A-Z]/.test(words[idx][0])) {
-      next = RiTa.capitalize(next); // keep capitals
-    }
-
-    console.log("replace(" + idx + "): " + word + " -> " + next);
-
-    words[idx] = next; // do replacement
-    var changes = document.getElementById("change");
-
-    changes.textContent = word;
-    var og_text = document.getElementById("word");
-
-    og_text.textContent = next;
-
-    break;
-  }
-
-  // recombine into string and display
-  txt = RiTa.untokenize(words);
-  background(255, 240, 245);
-  fill(47, 79, 79);
-  text("Having a Coke with You", 600, 100, 1000, height);
-  text("by Frank O'Hara", 630, 150, 1000, height);
-  text(txt, 300, 250, 900, height);
-
-  setTimeout(nextWord, 1500);
-}
+document.querySelector(".btn-reset").addEventListener("click", () => {
+  document.querySelector(".text").innerHTML = txt;
+});
